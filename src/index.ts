494b97b036f82e32d2460c31629488d17b232a2e
Ann00012 <ann.malchevska@gmail.com>
2026-09-19 19:23:00 +0300

    feat:update routing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { authRouter } from './auth/auth.controller.js';
 import { errorHandler } from './middleware/error-handler.js';
+import { verifyToken } from './middleware/authMiddleware.js';
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -18,7 +19,9 @@ app.use((_request, response, next) => {
 });
 
 app.use('/api', authRouter);
-
+app.get('/api/me', verifyToken, (req, res) => {
+  res.json({ message: 'Authorized access', userEmail: (req as any).userId });
+});
 app.get('/health', (_request, response) => {
   response.json({ status: 'ok', service: 'serial-tracker-backend' });
 });
