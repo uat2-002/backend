99ea7b8d42a62cabb5a665a064e269502ac3d597
Ann00012 <ann.malchevska@gmail.com>
2026-09-19 16:09:25 +0300

    feat: add repository for database operations

---
+++ src/watchlist/watchlist.repository.ts
@@ -0,0 +1,10 @@
+import { prisma } from '../prisma-client.js';
+
+export async function addSeriesToUser(userEmail: string, tmdbId: number) { 
+    return prisma.userSeries.create({
+        data: {
+            userEmail,
+            tmdbId
+        }
+    });
+}
