c6b10fe84eb7e63d919739a9ed28e4801836a4aa
Ann00012 <ann.malchevska@gmail.com>
2026-09-19 19:22:26 +0300

    feat:implement login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { registerUser } from './auth.service.js';
+import { registerUser,loginUser } from './auth.service.js';
 import { registerSchema } from './auth.schema.js';
 import { validate } from '../middleware/validate.js';
 
@@ -9,4 +9,15 @@ authRouter.post('/register', validate(registerSchema), async (req, res) => {
   const { email, password } = req.body;
   const user = await registerUser(email, password);
   res.status(201).json(user);
+});
+
+authRouter.post('/login', validate(registerSchema), async (req, res,next) => {
+  try {
+    const { email, password } = req.body;
+    const result = await loginUser(email, password);
+    res.status(200).json(result);
+   }
+  catch (error) {
+    next(error);
+   }
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import { findUserByEmail, createUser } from './auth.repository.js';
 import { HttpError } from '../errors/http-error.js';
+import jwt from 'jsonwebtoken';
 
 const SALT_ROUNDS = 10;
 
@@ -12,4 +13,19 @@ export async function registerUser(email: string, password: string) {
 
   const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
   return createUser(email, passwordHash);
+}
+
+export async function loginUser(email: string, password: string) {
+    const user = await findUserByEmail(email);
+    if (!user) {
+      throw new HttpError(401, 'Authentication failed');
+    }
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!passwordMatch) {
+      throw new HttpError(401, 'Authentication failed');
+    }
+    const accessToken = jwt.sign({ userId: user.email}, 'your-secret-key', {
+ expiresIn: '24h',
+    });
+  return { accessToken };
 }
